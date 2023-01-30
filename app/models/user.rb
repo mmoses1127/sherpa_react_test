29ce60eea379fc67e2f40f8966d2538de5c7ac00
Michael <mmoses1127@gmail.com>
2023-01-29 23:45:30 -0800

    create user migration and model

---
+++ app/models/user.rb
@@ -0,0 +1,41 @@
+class User < ApplicationRecord
+  has_secure_password
+  before_validation :ensure_session_token
+
+  validates :email, presence: true, uniqueness: true
+  validates :session_token, presence: true, uniqueness: true
+  validates :password, length: { in: 6..255 }, allow_nil: true
+  
+
+  def self.find_by_credentials(email, password)
+    user = nil
+    user = User.find_by(email: email)
+
+    if user && user.authenticate(password)
+      return user
+    else
+      return nil 
+    end
+  end
+
+  def reset_session_token!
+    self.session_token = generate_session_token
+    self.save!
+    self.session_token
+  end
+
+  private
+
+  def generate_session_token
+    while true
+      token = SecureRandom::urlsafe_base64(16)
+      return token unless User.exists?(session_token: token)
+    end
+  end
+
+  def ensure_session_token
+    self.session_token ||= generate_session_token
+  end
+
+
+end
